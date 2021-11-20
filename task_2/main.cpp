80ba181702f57e1f48e8661bedf4f1dbd7a4b8f1
Chuviliaeva Irina <chuviliaeva.ip@phystech.edu>
2021-11-20 14:14:24 +0300

    1st and 2nd algs -- need to improve borders

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -15,18 +15,29 @@ class Huang_hist {
   int bins[bins_num];
   int before_med_sum = 0;
   uint8_t current_med = 0;
-  int window_size, r;
+  int r, window_size;
 
   public:
-  Huang_hist(const int sz, const int filter_r){
-    window_size = sz;
+  Huang_hist(const int filter_r){
     r = filter_r;
-    for(int i = 0; i < bins_num; ++i){
-      bins[i] = 0;
-    }
+    window_size = (2 * r + 1) * (2 * r + 1);
   }
 
   ~Huang_hist() = default;
+
+  void to_string(){
+    std::cout << "bins:" << std::endl;
+    for(int i = 0; i < bins_num; ++i){
+      if(bins[i] != 0){
+        for(int t = 0; t < bins[i]; ++t) {
+          std::cout << i << " ";
+        }
+      }
+    }
+    std::cout << std::endl;
+    std::cout << "med = " << static_cast<int>(current_med) << std::endl;
+    std::cout << "before_med_sum = " << before_med_sum << std::endl;
+  }
 
   uint8_t find_med_first_time(const uint8_t* window){
     for(int i = 0; i < bins_num; ++i){
@@ -84,10 +95,127 @@ class Huang_hist {
 class Perrault_hist{
 private:
   static const int bins_num = MAX_UINT8_T + 1;
+  int current_hist[bins_num];
   uint8_t current_med = 0;
-  int window_size, r, m;
+  int before_med_sum = 0;
+  int n, m, r, window_size;
 
+  int** rows_hists;
+  int row_ind = 0;
+  int string_ind = 0;
+  uint8_t* pixels;
 
+public:
+  Perrault_hist(const int filter_r, const cv::Mat& image){
+    r = filter_r;
+    window_size = (2 * r + 1) * (2 * r + 1);
+    cv::Size sz = image.size();
+    n = sz.height;
+    m = sz.width;
+    pixels = reinterpret_cast<uint8_t*>(image.data);
+
+    // Создаём гистограммы столбцов
+    rows_hists = new int*[m];
+    for(int j = 0; j < m; ++j){
+      rows_hists[j] = new int[bins_num];
+      for(int i = 0; i < bins_num; ++i){
+        rows_hists[j][i] = 0;
+      }
+      for(int i = 0; i < 2 * r + 1; ++i){
+        rows_hists[j][pixels[i * m + j]]++;
+      }
+    }
+  }
+
+  ~Perrault_hist(){
+    // нужно очистить память, которую выделили через new
+    for(int i = 0; i < m; ++i){
+      delete[] rows_hists[i];
+    }
+    delete[] rows_hists;
+  }
+
+  uint8_t first_med_in_row(){
+    before_med_sum = 0;
+    for(int i = 0; i < bins_num; ++i){
+      current_hist[i] = 0;
+    }
+
+    for(int j = 0; j < 2 * r + 1; ++j){
+      for(int i = 0; i < bins_num; ++i){
+        current_hist[i] += rows_hists[j][i];
+      }
+    }
+
+    for(int i = 0; i < bins_num; ++i){
+      if(before_med_sum + current_hist[i] >= (window_size + 1) / 2){
+        current_med = static_cast<uint8_t>(i);
+        break;
+      }
+      before_med_sum += current_hist[i];
+    }
+    return current_med;
+  }
+
+  uint8_t next_median(){
+    //std::cout << "next_median: row_ind = " << row_ind << ", string_ind = " << string_ind << std::endl;
+    if(row_ind == 0){
+      // началась новая строка
+      if(string_ind != 0){
+        // надо "опустить" первые 2r + 1 столбцов
+        for(int j = 0; j < 2 * r + 1; ++j){
+          rows_hists[j][pixels[(string_ind - 1) * m + j]]--;
+          rows_hists[j][pixels[(string_ind + 2 * r) * m + j]]++;
+        }
+      }
+      //std::cout << "done downing of rows" << std::endl;
+      row_ind++;
+      return first_med_in_row();
+    }
+
+    // переход в строке
+    // перемещаем один пиксель в гистограмме столбца
+    int outcoming_row = row_ind - 1;
+    int incoming_row = row_ind + 2 * r + 1;
+    if(string_ind != 0){
+      rows_hists[incoming_row][pixels[(string_ind - 1) * m + incoming_row]]--;
+      rows_hists[incoming_row][pixels[(string_ind + 2 * r) * m + incoming_row]]++;
+    }
+
+    // Добавляем и удаляем столбцы в гистограмме. Тут же пересчитываем сумму перед медианой
+    for(int i = 0; i < bins_num; ++i){
+      current_hist[i] += rows_hists[incoming_row][i];
+      current_hist[i] -= rows_hists[outcoming_row][i];
+      if(i < static_cast<int>(current_med)){
+        before_med_sum += rows_hists[incoming_row][i];
+        before_med_sum -= rows_hists[outcoming_row][i];
+      }
+    }
+
+    row_ind++;
+    if(row_ind == m - (2 * r) - 1){
+      row_ind = 0;
+      string_ind++;
+    }
+
+    // Обновляем сумму перед медианой и саму медиану как в алгоритме
+    if(before_med_sum > window_size / 2){
+      while(before_med_sum > window_size / 2){
+        current_med--;
+        before_med_sum -= current_hist[static_cast<int>(current_med)];
+      }
+      return current_med;
+    }
+
+    if(before_med_sum + current_hist[static_cast<int>(current_med)] <= window_size / 2){
+      while(before_med_sum + current_hist[static_cast<int>(current_med)] <= window_size / 2){
+        before_med_sum += current_hist[static_cast<int>(current_med)];
+        current_med++;
+      }
+      return current_med;
+    }
+
+    return current_med;}
 };
 
 uint8_t find_med_by_sort(uint8_t* window, const int filter_r) {
@@ -98,7 +226,42 @@ uint8_t find_med_by_sort(uint8_t* window, const int filter_r) {
   return window[size / 2];
 }
 
-cv::Mat huang_filter(cv::Mat original_image, const int filter_r) {
+cv::Mat naive_filter(const cv::Mat& original_image, const int filter_r){
+  int n, m;
+  cv::Size sz = original_image.size();
+  n = sz.height;
+  m = sz.width;
+  uint8_t* origin_pixels = reinterpret_cast<uint8_t*>(original_image.data);
+  uint8_t* new_pixels = new uint8_t[n * m];
+  uint8_t filter_window[(2 * filter_r + 1) * (2 * filter_r + 1)];
+
+  for(int i = 0; i < n; ++i){
+    for(int j = 0; j < m; ++j){
+      new_pixels[i * m + j] = 0;
+    }
+  }
+  for(int i = 0; i < n - (2 * filter_r + 1); ++i){
+    for(int j = 0; j < m - (2 * filter_r + 1); ++j){
+      for(int x = 0; x < 2 * filter_r + 1; ++x){
+        for(int y = 0; y < 2 * filter_r + 1; ++y){
+          filter_window[x * (2 * filter_r + 1) + y] = origin_pixels[(i + x) * m + (j + y)];
+          new_pixels[(i + filter_r + 1) * m + (j + filter_r + 1)] = find_med_by_sort(filter_window, filter_r);
+        }
+      }
+      std::cout << "filter_window:" << std::endl;
+      for(int x = 0; x < 2 * filter_r + 1; ++x){
+        for(int y = 0; y < 2 * filter_r + 1; ++y){
+          std::cout << static_cast<int>(filter_window[x * (2 * filter_r + 1) + y]) << " ";
+        }
+      }
+      std::cout << std::endl;
+      std::cout << "med = " << static_cast<int>(new_pixels[(i + filter_r + 1) * m + (j + filter_r + 1)]) << std::endl;
+    }
+  }
+  return cv::Mat(n, m, CV_8UC1, new_pixels);
+}
+
+cv::Mat huang_filter(cv::Mat& original_image, const int filter_r) {
   int n, m;
   cv::Size sz = original_image.size();
   n = sz.height;
@@ -113,7 +276,7 @@ cv::Mat huang_filter(cv::Mat original_image, const int filter_r) {
     }
   }
 
-  Huang_hist hist = Huang_hist(window_size, filter_r);
+  Huang_hist hist = Huang_hist(filter_r);
   uint8_t *first_time_input = new uint8_t[window_size];
   uint8_t *row_input = new uint8_t[2 * filter_r + 1];
   uint8_t *row_output = new uint8_t[2 * filter_r + 1];
@@ -134,12 +297,43 @@ cv::Mat huang_filter(cv::Mat original_image, const int filter_r) {
         }
         new_pixels[(i + filter_r + 1) * m + (j + filter_r + 1)] = hist.find_med_in_row(row_output, row_input);
       }
+      hist.to_string();
     }
   }
+  delete[] first_time_input;
+  delete[] row_input;
+  delete[] row_output;
   return cv::Mat(n, m, CV_8UC1, new_pixels);
 }
 
+cv::Mat perrault_filter(cv::Mat& original_image, const int filter_r) {
+  int n, m;
+  cv::Size sz = original_image.size();
+  n = sz.height;
+  m = sz.width;
+  uint8_t *new_pixels = new uint8_t[n * m];
 
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      new_pixels[i * m + j] = 0;
+    }
+  }
+
+  Perrault_hist phist = Perrault_hist(filter_r, original_image);
+  //std::cout << "in perrault_filter: after phist creation" << std::endl;
+  //std::cout << "n = " << n << ", m = " << m << std::endl;
+  for (int i = 0; i < n - (2 * filter_r + 1); ++i) {
+    for (int j = 0; j < m - (2 * filter_r + 1); ++j) {
+      //std::cout << "i = " << i << ", j = " << j << std::endl;
+      new_pixels[(i + filter_r + 1) * m + (j + filter_r + 1)] = phist.next_median();
+      if(i == 2 && j == 2){
+        std::cout << "in perrault filter" << std::endl;
+      }
+    }
+  }
+
+  return cv::Mat(n, m, CV_8UC1, new_pixels);
+}
 
 /* void test_med(int size = 9) {
   uint8_t first_input[9] = {255, 255, 255, 255, 255, 255, 255, 255, 255};
@@ -188,36 +382,10 @@ cv::Mat huang_filter(cv::Mat original_image, const int filter_r) {
 
 
 
-cv::Mat naive_filter(const cv::Mat& original_image, const int filter_r){
-  int n, m;
-  cv::Size sz = original_image.size();
-  n = sz.height;
-  m = sz.width;
-  uint8_t* origin_pixels = reinterpret_cast<uint8_t*>(original_image.data);
-  uint8_t* new_pixels = new uint8_t[n * m];
-  uint8_t filter_window[(2 * filter_r + 1) * (2 * filter_r + 1)];
-
-  for(int i = 0; i < n; ++i){
-    for(int j = 0; j < m; ++j){
-      new_pixels[i * m + j] = 0;
-    }
-  }
-  for(int i = 0; i < n - (2 * filter_r + 1); ++i){
-    for(int j = 0; j < m - (2 * filter_r + 1); ++j){
-      for(int x = 0; x < 2 * filter_r + 1; ++x){
-        for(int y = 0; y < 2 * filter_r + 1; ++y){
-          filter_window[x * (2 * filter_r + 1) + y] = origin_pixels[(i + x) * m + (j + y)];
-          new_pixels[(i + filter_r + 1) * m + (j + filter_r + 1)] = find_med_by_sort(filter_window, filter_r);
-        }
-      }
-    }
-  }
-  return cv::Mat(n, m, CV_8UC1, new_pixels);
-}
 
 int main() {
 
-  std::string input_path = "../images/stones.jpg";
+  /*std::string input_path = "../images/trees.jpg";
   cv::Mat origin = cv::imread(input_path, cv::IMREAD_GRAYSCALE);
   if(origin.empty())
   {
@@ -226,10 +394,18 @@ int main() {
   }
 
   std::cout << "channels = " << origin.channels() << std::endl;
-  std::cout << "type = " << origin.type() << std::endl;
+  std::cout << "type = " << origin.type() << std::endl;*/
 
+  uint8_t orig_pixel[5 * 5];
+  for(int i = 0; i < 5; ++i){
+    for(int j = 0; j < 5; ++j){
+      orig_pixel[i * 5 + j] = static_cast<uint8_t>(rand() % 256);
+    }
+  }
+  cv::Mat origin = cv::Mat(5, 5, CV_8UC1, orig_pixel);
   cv::Mat naive_filtered = naive_filter(origin, 1);
   cv::Mat huang_filtered = huang_filter(origin, 1);
+  cv::Mat perrault_filtered = perrault_filter(origin, 1);
 
   cv::namedWindow("origin image", cv::WINDOW_NORMAL); // cv::WINDOW_AUTOSIZE
   imshow("origin image", origin);
@@ -240,8 +416,32 @@ int main() {
   cv::namedWindow("huang filtered image", cv::WINDOW_NORMAL); // cv::WINDOW_AUTOSIZE
   imshow("huang filtered image", huang_filtered);
 
+  cv::namedWindow("perrault filtered image", cv::WINDOW_NORMAL); // cv::WINDOW_AUTOSIZE
+  imshow("perrault filtered image", perrault_filtered);
 
   cv::waitKey(0);
 
+  uint8_t* naive_pixel = reinterpret_cast<uint8_t*>(naive_filtered.data);
+  uint8_t* huang_pixel = reinterpret_cast<uint8_t*>(huang_filtered.data);
+  uint8_t* perrault_pixel = reinterpret_cast<uint8_t*>(perrault_filtered.data);
+  cv::Size sz = naive_filtered.size();
+  int n = sz.height;
+  int m = sz.width;
+  /*for(int i = 0; i < 10; ++i){
+    for(int j = 0; j < m; ++j){
+      if(huang_pixel[i * m + j] != naive_pixel[i * m + j]){
+        std::cout << "ALERT in huang with naive, i = " << i << ", j = " << j << std::endl;
+        std::cout << "huang value = " << static_cast<int>(huang_pixel[i * m + j]) << std::endl;
+        std::cout << "naive value = " << static_cast<int>(naive_pixel[i * m + j]) << std::endl;
+
+      }
+      if(perrault_pixel[i * m + j] != naive_pixel[i * m + j]){
+        std::cout << "ALERT in perrault with naive i = " << i << ", j = " << j << std::endl;
+        std::cout << "perrault value = " << static_cast<int>(huang_pixel[i * m + j]) << std::endl;
+        std::cout << "naive value = " << static_cast<int>(naive_pixel[i * m + j]) << std::endl;
+
+      }
+    }
+  }*/
   return 0;
 }
